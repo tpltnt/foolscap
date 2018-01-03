0d075b7c95cd6ffdd12a11845b711d919c3a56cc
tpltnt <tpltnt.github@dropcut.net>
2018-01-04 00:59:08 +0100

    basic port of unicode

--- src/foolscap/slicers/unicode.py
+++ src/foolscap/slicers/unicode.py
@@ -8,7 +8,8 @@ from foolscap.constraint import OpenerConstraint, Any
 
 class UnicodeSlicer(BaseSlicer):
     opentype = ("unicode",)
-    slices = unicode
+    slices = str
+
     def sliceBody(self, streamable, banana):
         yield self.obj.encode("UTF-8")
 
@@ -35,10 +36,11 @@ class UnicodeUnslicer(LeafUnslicer):
         assert ready_deferred is None
         if self.string != None:
             raise BananaError("already received a string")
-        self.string = unicode(obj, "UTF-8")
+        self.string = str(obj, "UTF-8")
 
     def receiveClose(self):
         return self.string, None
+
     def describe(self):
         return "<unicode>"
 
@@ -72,7 +74,7 @@ class UnicodeConstraint(OpenerConstraint):
             self.regexp = re.compile(regexp)
 
     def checkObject(self, obj, inbound):
-        if not isinstance(obj, unicode):
+        if not isinstance(obj, str):
             raise Violation("not a unicode object")
         if self.maxLength != None and len(obj) > self.maxLength:
             raise Violation("string too long (%d > %d)" %
